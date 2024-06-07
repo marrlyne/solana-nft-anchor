da060a352547d441212ec78aeb11036822dc3e68
Marrlyne <marrlyne63@gmail.com>
2024-06-08 00:42:00 +0100

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("R2v9zQ4GMRKiRmVxLY9DZ9WgHqfKAoUMKxiA8H1G");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
